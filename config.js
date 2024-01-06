3289900cf51bdde81932f323e3bd6be4d9b69482
sarwarali12 <155801494+sarwarali12@users.noreply.github.com>
2024-01-06 16:25:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['03242895504', 'king bot 786', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
